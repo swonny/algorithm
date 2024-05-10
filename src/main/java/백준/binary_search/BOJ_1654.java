fe2cad5c159767915acff99f1cfc15758af03be3
swonny <wonny921@gmail.com>
2024-05-10 17:45:26 +0900

    24.05.10: 이진 탐색 BOJ_1654

---
+++ src/main/java/백준/binary_search/BOJ_1654.java
@@ -0,0 +1,42 @@
+package 백준.binary_search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_1654 {
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        long K = Long.parseLong(st.nextToken());
+        long[] arr = new long[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(solution(K, N, arr));
+    }
+
+    public static long solution(long K, long N, long[] lines) {
+        long left = 0, right = Arrays.stream(lines).max().getAsLong() + 1;
+
+        while (left < right) {
+            long mid = left + ((right - left) / 2);
+            long count = 0;
+            for (long l : lines) {
+                count += l / mid;
+            }
+            if (count < K) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left - 1;
+    }
+}
